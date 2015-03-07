698648cd67b4be560e210730385d31b72d71f97d
Akshay Budhkar <akshaybudhkar@gmail.com>
2015-03-07 11:50:35 -0500

    Initial Commit

---
+++ Chess/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Chess
+//
+//  Created by Akshay Budhkar on 2015-03-07.
+//  Copyright (c) 2015 Akshay Budhkar. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
